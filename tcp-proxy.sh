d6aaaaa469fdb479f19f00d19b9cf3124f6371b3
Thomas Deutsch <thomas@tuxpeople.org>
2023-12-07 13:10:55 +0100

    Update tcp-proxy.sh

--- tcp-proxy.sh
+++ tcp-proxy.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 2 ]; then
-    echo "usage: $(basename $0) host port"
+HOST="${TARGET_HOST:-${1}}"
+PORT="${TARGET_PORT:-${2}}"
+LISTEN_PORT=${LISTEN_PORT:-${PORT}}
+
+if [ -n ${HOST} ] && [ -n ${PORT} ] && [ -n ${LISTEN_PORT} ]; then
+    echo "Missing environment variables or command line options: $(basename $0) host port"
     exit 1
 fi
-
-HOST="$1"
-PORT="$2"
-
-LISTEN_PORT=${LISTEN_PORT:-${PORT}}
 
 echo "relay TCP/IP connections on :${LISTEN_PORT} to ${HOST}:${PORT}"
 exec socat TCP-LISTEN:${LISTEN_PORT},fork,reuseaddr TCP:${HOST}:${PORT}
